360868859a0185ec46f9e38f35324bcabc039de0
Gal Samama <galsamama@gmail.com>
2025-12-29 22:38:46 +0200

    Remove async and dynamic export from home page - make it static

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { HomeContent } from '@/components/home/HomeContent';
 
-export const dynamic = 'force-dynamic';
-
-export default async function Home() {
+export default function Home() {
   // כל התכנים סטטיים - ללא Supabase
   const programs: any[] = [];
   
