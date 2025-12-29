79c83656bd62abaf84201390068161492b515855
Gal Samama <galsamama@gmail.com>
2025-12-29 22:36:20 +0200

    Add dynamic export to home page to fix 404 error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { HomeContent } from '@/components/home/HomeContent';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   // כל התכנים סטטיים - ללא Supabase
   const programs: any[] = [];
