f5a761e7cefce0b9420abf225a7530fcbee483ab
Gal Samama <galsamama@gmail.com>
2025-12-29 21:35:57 +0200

    Add onClick prop support to Card component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -4,13 +4,15 @@ interface CardProps {
   children: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-export const Card: React.FC<CardProps> = ({ children, className = '', style }) => {
+export const Card: React.FC<CardProps> = ({ children, className = '', style, onClick }) => {
   return (
     <div 
       className={`bg-white rounded-xl p-6 md:p-8 border border-secondary/10 shadow-sm hover:shadow-md transition-shadow duration-300 ${className}`}
       style={style}
+      onClick={onClick}
     >
       {children}
     </div>
